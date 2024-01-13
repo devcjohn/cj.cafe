16afdd18751644a6356bdf6dc9b73fee48191a65
Chuck Johnson <devcjohn@gmail.com>
2024-01-13 14:53:52 -0600

    Refactor -  separate tf into multiple files

---
+++ tf/iam.tf
@@ -0,0 +1,46 @@
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+/* A policy for creating ClowdWatch logs */
+resource "aws_iam_policy" "ecs_logging" {
+  name        = "ecs_logging"
+  description = "Allow ECS tasks to create and write to CloudWatch Logs groups"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_logging_attachment" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = aws_iam_policy.ecs_logging.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
---
+++ tf/load_balancer.tf
@@ -0,0 +1,29 @@
+resource "aws_lb" "app" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_web.id]
+  subnets            = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
+}
+
+resource "aws_lb_target_group" "app" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP" /* the protocol used between the load balancer and the registered targets does not need to be https */
+  vpc_id   = aws_vpc.my-vpc.id
+  target_type = "ip" // The default is "instance", but this must be "ip" because the task is running in "awsvpc" network mode
+  
+}
+
+resource "aws_lb_listener" "https-secure" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = module.acm.acm_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
