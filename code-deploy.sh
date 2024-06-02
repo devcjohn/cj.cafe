ea28c384a37ef26a318ee8b7e0e1ae212a2c564b
Charles Johnson <devcjohn@gmail.com>
2024-06-02 16:27:24 -0500

    Add cache invalidation on code deploy so updated website comes up faster

--- code-deploy.sh
+++ code-deploy.sh
@@ -1,2 +1,8 @@
+#!/bin/sh
+
+# Load environment variables from .env file
+. ./.env
+
 npm run build
-aws s3 sync ./dist s3://cj-cafe-bucket-5
+aws s3 sync ./dist s3://"$AWS_BUCKET_NAME" --delete
+aws cloudfront create-invalidation --distribution-id  "$AWS_CLOUDFRONT_DISTRIBUTION_ID" --paths "/*"
